c3a10eea0d0f1184de0aed1ca944a132e980b3e2
liuchong <884368405@qq.com>
2022-06-11 18:10:27 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+   <Avatar url="http://mdrs.yuanjin.tech/FgMwAPYq17So9nwVH44ltDHo7u3c" :size="150"/>
+  </div>
+</template>
+
+<script>
+import Avatar from './components/Avatar'
+export default {
+  name: 'App',
+  components: {
+    Avatar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+// 入口文件
+import Vue from 'vue'
+import App from './App.vue'
+
+
+
+new Vue({
+  render: h => h(App), // 渲染实例并挂载
+}).$mount('#app')
